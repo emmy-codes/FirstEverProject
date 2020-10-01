962a021a45f2a9d69a672e8bd52e85a9d34051a7
Emma Scott-Allen <alunia.moon@gmail.com>
2020-10-01 17:57:10 +0200

    Fix URL path to include /FirstEverProject/

--- game.js
+++ game.js
@@ -7,11 +7,11 @@ let inputField = document.querySelector('.input');
 let form = document.querySelector('.form');
 let page = document.querySelector('.gamebox');
 
-let page1URL = 'mainForest.html';
-let page2URL = 'ruinsSouth.html';
-let page3URL = 'streamWest.html';
-let page4URL = 'wallEast.html';
-let page5URL = 'cottageNorth.html';
+let page1URL = '/FirstEverProject/mainForest.html';
+let page2URL = '/FirstEverProject/ruinsSouth.html';
+let page3URL = '/FirstEverProject/streamWest.html';
+let page4URL = '/FirstEverProject/wallEast.html';
+let page5URL = '/FirstEverProject/cottageNorth.html';
 
 let pagesOptions = {
     page1: { // mainForest
